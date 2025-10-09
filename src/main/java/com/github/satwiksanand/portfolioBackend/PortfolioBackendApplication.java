0f834918feae8fb07c0fe9f06a8b49780c526134
satwik sanand <sanandsatwik421@gmail.com>
2025-10-09 06:50:26 +0000

    basic spring project setup

---
+++ src/main/java/com/github/satwiksanand/portfolioBackend/PortfolioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.satwiksanand.portfolioBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/github/satwiksanand/portfolioBackend/models/Projects.java
@@ -0,0 +1,4 @@
+package com.github.satwiksanand.portfolioBackend.models;
+
+public class Projects {
+}
